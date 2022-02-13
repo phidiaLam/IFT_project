b1c3dd05e31aacb919fafd99e57081fb469f363c
lwt-414 <2957035701@qq.com>
2022-02-13 23:10:38 +0800

    💄  add home page UI ✨  add function "upload file"

---
+++ src/components/navbar.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="navbar">
+    <el-menu
+      :default-active="$route.path"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="/">Home</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+ 
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app-main">
+    <navbar></navbar>
+    <el-upload
+      drag
+      :auto-upload="false"
+      action="#"
+      :on-change="importData"
+      ref="upload"
+      accept="application/json"
+      :show-file-list="false"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">
+        Drag the file here, or <em>click to upload</em>
+      </div>
+    </el-upload>
+  </div>
+</template>
+ 
+<script>
+import Navbar from "../../components/navbar.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      fileData: "",
+    };
+  },
+  components: {
+    Navbar,
+  },
+  methods: {
+    importData(file) {
+      var suffixIndex = file.name.lastIndexOf(".");
+      var fileSuffix = file.name.substr(suffixIndex + 1);
+      //输出结果
+      console.log(fileSuffix);
+      const reader = new FileReader();
+      reader.onload = (res) => {
+        const { result } = res.target; // get the string from files
+      };
+      reader.readAsText(new Blob([file.raw]), "utf-8"); // Parse using UTF-8
+    },
+  },
+};
+</script>
+ 
+<style lang="less" scoped>
+#app-main {
+}
+</style>
