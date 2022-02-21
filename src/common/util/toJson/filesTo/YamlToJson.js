dedc334cd5dbd0b197a7ce1f097cc9c22bf6c395
lwt-414 <2957035701@qq.com>
2022-02-21 13:49:42 +0800

    ✨ complete yaml to json

--- src/common/util/toJson/filesTo/YamlToJson.js
+++ src/common/util/toJson/filesTo/YamlToJson.js
@@ -1,8 +1,15 @@
 import { FilesToJson } from "../FilesToJson";
+import yaml from "js-yaml"
 
 export class YamlToJson extends FilesToJson {
 
     constructor(inputText) {
         super(inputText)
+    }
+
+    async changeToJson() {
+        let json;
+        json = yaml.load(this.inputText);
+        this.jsonText = JSON.stringify(json);
     }
 }
