fa4f1f6a1abbe90a8a0bfb42528a5fe69bc1370b
lwt-414 <2957035701@qq.com>
2022-03-26 14:19:13 +0800

    🐛 fix bug of handleToMulti 🚧 add function of handleToSingle

--- src/common/util/toStructFormat.js
+++ src/common/util/toStructFormat.js
@@ -1,6 +1,45 @@
 // process multi-level object to single array, and it will be used to convert to a single file
-function singleArray(object) {
+function singleArray(jsonObj) {
+    let handledJson = handleToSingle(jsonObj);
+    return handledJson;
+}
 
+// process mult-level object to single arrays, and it will be used to convert to a single file
+function handleToSingle(jsonObj, parentNode = null, parentArray = -1) {
+    // Check if it's an array
+    let isArray = false;
+    if (jsonObj instanceof Array) {
+        isArray = true;
+    }
+
+    // Determine which parent element is an array
+    if (parentArray >= 0) {
+        parentArray -= 1
+    }
+    let newObject;
+    // Iterate over each child element
+    for (let key in jsonObj) {
+        // Recursively handle different cases
+        if (jsonObj[key] instanceof Array) {
+            newObject = handleToSingle(jsonObj[key], mulitArrayObj, key, 2)
+        } else if (typeof (jsonObj[key]) == 'object' && parentArray == 1) {
+            newObject = handleToSingle(jsonObj[key], mulitArrayObj, parentNode, parentArray)
+        } else if (typeof (jsonObj[key]) == 'object' && parentArray <= 0) {
+            newObject = handleToSingle(jsonObj[key], mulitArrayObj, key, parentArray)
+            for (let keyNew in newObject) {
+                jsonObj[keyNew] = JSON.parse(JSON.stringify(newObject[keyNew]))
+            }
+            delete jsonObj[key]
+        }  else {
+            jsonObj[parentNode+key]=JSON.parse(JSON.stringify(jsonObj[key]));
+            delete jsonObj[key];
+        } 
+    }
+
+    let changedObject;
+    
+
+    return changedObject;
 }
 
 // Passing in a JSON format object and return json with multiple array.
@@ -37,9 +76,9 @@ function handleToMulti(jsonObj, mulitArrayObj, parentPath = null, parentArray = 
         // Recursively handle different cases
         if (jsonObj[key] instanceof Array) {
             jsonObj[key] = handleToMulti(jsonObj[key], mulitArrayObj, path, 2) + "(table)"
-        } else if (typeof (jsonObj[key]) == 'object' && parentArray < 0) {
+        } else if (typeof (jsonObj[key]) == 'object' && parentArray <= 0) {
             jsonObj[key] = handleToMulti(jsonObj[key], mulitArrayObj, path) + "(table)"
-        } else if (typeof (jsonObj[key]) == 'object' && parentArray >= 0) {
+        } else if (typeof (jsonObj[key]) == 'object' && parentArray > 0) {
             jsonObj[key] = handleToMulti(jsonObj[key], mulitArrayObj, path, parentArray)
         }
     }
@@ -55,7 +94,12 @@ function handleToMulti(jsonObj, mulitArrayObj, parentPath = null, parentArray = 
     } else if (parentArray < 0) {
         let arr = [];
         arr.push(jsonObj)
-        mulitArrayObj[parentPath] = arr;
+        
+        if(mulitArrayObj[parentPath]) {
+            mulitArrayObj[parentPath] = mulitArrayObj[parentPath].concat(arr)
+        } else {
+            mulitArrayObj[parentPath] = arr;
+        }
     } else {
         parentPath = jsonObj
     }
